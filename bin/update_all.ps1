def46dd51d7fc32f7b019faec3ccc7ef8f414919
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-06-25 17:26:39 +0200

    Add Auto Updater action

---
+++ bin/update_all.ps1
@@ -0,0 +1,22 @@
+param(
+    [string]$ForcedPackages = ''
+)
+
+$ErrorActionPreference = 'Stop'
+
+# Configure AU execution environment
+$Env:au_Push = $true
+$Env:au_Force = $false
+
+# If forced packages are specified (e.g. "vscode-yaml"), AU will ignore versions and force update them
+if ($ForcedPackages) {
+    $Env:au_Force = $true
+    $Env:au_Packages = $ForcedPackages
+}
+
+# The main AU command to execute the update.ps1 scripts in all subfolders
+Update-AUPackages -Options @{
+    Threads = 3
+    # Use standard chocolatey community push
+    PushURL = 'https://push.chocolatey.org/'
+}
