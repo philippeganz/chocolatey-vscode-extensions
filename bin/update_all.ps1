a9cbc0292ed15845f317251be3e639e50bbb9a93
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-06-26 10:32:50 +0200

    Use correct boolean format

--- bin/update_all.ps1
+++ bin/update_all.ps1
@@ -5,12 +5,12 @@ param(
 $ErrorActionPreference = 'Stop'
 
 # Configure AU execution environment
-$Env:au_Push = $true
-$Env:au_Force = $false
+$Env:au_Push = 'true'
+$Env:au_Force = 'false'
 
 # If forced packages are specified (e.g. "vscode-yaml"), AU will ignore versions and force update them
 if ($ForcedPackages) {
-    $Env:au_Force = $true
+    $Env:au_Force = 'true'
     $Env:au_Packages = $ForcedPackages
 }
 
