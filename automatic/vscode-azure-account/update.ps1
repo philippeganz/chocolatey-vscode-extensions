de49ed7a10567e530484ffe4b13099b3012f58df
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:29:36 +0200

    Add new ms-vscode.azure-account extension

---
+++ automatic/vscode-azure-account/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-vscode"
+$ExtensionName = "azure-account"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
