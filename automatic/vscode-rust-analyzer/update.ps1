6bb3fb977086d84af0200c922f737196d371d254
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:33:05 +0200

    Add new rust-lang.rust-analyzer extension

---
+++ automatic/vscode-rust-analyzer/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "rust-lang"
+$ExtensionName = "rust-analyzer"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
