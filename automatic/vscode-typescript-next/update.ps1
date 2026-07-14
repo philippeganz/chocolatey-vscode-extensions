50b64617a919c5f25c6c78ee80229597d41f3a89
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:29:51 +0200

    Add new ms-vscode.vscode-typescript-next extension

---
+++ automatic/vscode-typescript-next/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-vscode"
+$ExtensionName = "vscode-typescript-next"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
