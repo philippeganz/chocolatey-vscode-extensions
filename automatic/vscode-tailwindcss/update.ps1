3f5bd3dde8044bd956c51093ae43fc4d46f70dca
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:25:50 +0200

    Add new bradlc.vscode-tailwindcss extension

---
+++ automatic/vscode-tailwindcss/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "bradlc"
+$ExtensionName = "vscode-tailwindcss"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
