67178587d3218ddf8163a94beb5b1dcb28401bea
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:30:12 +0200

    Add new naumovs.color-highlight extension

---
+++ automatic/vscode-color-highlight/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "naumovs"
+$ExtensionName = "color-highlight"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
