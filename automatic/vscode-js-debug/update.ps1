878c3601dbf5fee427dddbb03dc738d060ea667f
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:30:06 +0200

    Add new msjsdiag.vscode-react-native extension

---
+++ automatic/vscode-js-debug/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-vscode"
+$ExtensionName = "js-debug"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
---
+++ automatic/vscode-react-native/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "msjsdiag"
+$ExtensionName = "vscode-react-native"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
