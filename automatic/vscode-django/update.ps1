91e5b2a53dcbc2b8b5d3cc3eb3571551b3c9da49
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:25:42 +0200

    Add new batisteo.vscode-django extension

---
+++ automatic/vscode-django/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "batisteo"
+$ExtensionName = "vscode-django"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
