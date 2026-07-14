3788630642ea655ec00143d0a4a3e72a332fd4e5
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:32:37 +0200

    Add new openai.chatgpt extension

---
+++ automatic/vscode-chatgpt/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "openai"
+$ExtensionName = "chatgpt"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
