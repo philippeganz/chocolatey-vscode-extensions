2b00a75364c72ef7a4b3f3c0c6b015ef146e372a
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:32:57 +0200

    Add new rangav.vscode-thunder-client extension

---
+++ automatic/vscode-thunder-client/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "rangav"
+$ExtensionName = "vscode-thunder-client"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
