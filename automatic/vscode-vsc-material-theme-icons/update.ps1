a99df09dab239215032abb4454b69cde48cf2749
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:36 +0200

    Add new equinusocio.vsc-material-theme-icons extension

---
+++ automatic/vscode-vsc-material-theme-icons/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "equinusocio"
+$ExtensionName = "vsc-material-theme-icons"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
