f5c73bde9fd0f40560cbdfb5376a635b97253066
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:30:24 +0200

    Add new octref.vetur extension

---
+++ automatic/vscode-vetur/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "octref"
+$ExtensionName = "vetur"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
