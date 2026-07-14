30ca469c822a4f69fb00da78c43840b37deb51fa
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:29:02 +0200

    Add new ms-ceintl.vscode-language-pack-zh-hans extension

---
+++ automatic/vscode-language-pack-zh-hans/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-ceintl"
+$ExtensionName = "vscode-language-pack-zh-hans"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
