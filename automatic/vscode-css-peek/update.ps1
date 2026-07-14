be13a7946ef85d92ede35504fe5a8560fe0f4382
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:32:54 +0200

    Add new pranaygp.vscode-css-peek extension

---
+++ automatic/vscode-css-peek/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "pranaygp"
+$ExtensionName = "vscode-css-peek"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
