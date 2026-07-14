89a44b3a3074ebe1c18687202e472be71b0a9074
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:29:44 +0200

    Add new ms-vscode.live-server extension

---
+++ automatic/vscode-live-server/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-vscode"
+$ExtensionName = "live-server"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
