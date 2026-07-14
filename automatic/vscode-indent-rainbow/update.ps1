3d946c9b3b3772876b4b916a9df41d35512a42e5
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:30:29 +0200

    Add new oderwat.indent-rainbow extension

---
+++ automatic/vscode-indent-rainbow/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "oderwat"
+$ExtensionName = "indent-rainbow"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
