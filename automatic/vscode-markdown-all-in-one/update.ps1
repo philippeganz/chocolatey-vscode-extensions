390107ac70e943322ac69861f1105cae3808d6b2
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:34:05 +0200

    Add new yzhang.markdown-all-in-one extension

---
+++ automatic/vscode-markdown-all-in-one/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "yzhang"
+$ExtensionName = "markdown-all-in-one"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
