de90ab2d2f578912a6bf77b8620aded5e82d5fb5
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:33:44 +0200

    Add new vscodevim.vim extension

---
+++ automatic/vscode-vim/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "vscodevim"
+$ExtensionName = "vim"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
