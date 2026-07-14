c2d3f3414c399cef1d1b93f9df551474880621f0
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:34:13 +0200

    Add new zignd.html-css-class-completion extension

---
+++ automatic/vscode-html-css-class-completion/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "zignd"
+$ExtensionName = "html-css-class-completion"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
