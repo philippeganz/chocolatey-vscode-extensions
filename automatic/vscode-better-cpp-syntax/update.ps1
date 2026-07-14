d8d05a30c9c7e1c76b564d9d0bb66f0de3eaab15
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:28:22 +0200

    Add new jeff-hykin.better-cpp-syntax extension

---
+++ automatic/vscode-better-cpp-syntax/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "jeff-hykin"
+$ExtensionName = "better-cpp-syntax"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
