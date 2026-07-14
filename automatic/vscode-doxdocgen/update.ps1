70db5d3e19950e76d0b0a1aacfa796e1a8b81389
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:26:14 +0200

    Add new cschlosser.doxdocgen extension

---
+++ automatic/vscode-doxdocgen/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "cschlosser"
+$ExtensionName = "doxdocgen"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
