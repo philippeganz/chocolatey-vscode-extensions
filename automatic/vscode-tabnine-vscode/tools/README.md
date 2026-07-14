98a9894e8dbd9e35d1b9533670b6f78149e1f70b
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:33:24 +0200

    Add new tabnine.tabnine-vscode extension

---
+++ automatic/vscode-tabnine-vscode/tools/README.md
@@ -0,0 +1,1 @@
+Tabnine does not onboard new users to this plugin. If you are interested in our enterprise solution, please contact [email removed]
---
+++ automatic/vscode-tabnine-vscode/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "tabnine"
+$ExtensionName = "tabnine-vscode"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
