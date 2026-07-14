62cb402da562a9bac5d381f85411f1612dc9de96
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:30:09 +0200

    Add new mtxr.sqltools extension

---
+++ automatic/vscode-sqltools/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "mtxr"
+$ExtensionName = "sqltools"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
