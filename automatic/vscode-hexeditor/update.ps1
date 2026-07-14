56990bcf3977e808789eb8837b0ebec0a27f9693
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:29:41 +0200

    Add new ms-vscode.hexeditor extension

---
+++ automatic/vscode-hexeditor/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-vscode"
+$ExtensionName = "hexeditor"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
