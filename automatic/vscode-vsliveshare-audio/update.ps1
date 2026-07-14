9668be4fa9ee2b9da69d2d8aa7da7d483d775b52
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:29:56 +0200

    Add new ms-vsliveshare.vsliveshare-audio extension

---
+++ automatic/vscode-vsliveshare-audio/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-vsliveshare"
+$ExtensionName = "vsliveshare-audio"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
