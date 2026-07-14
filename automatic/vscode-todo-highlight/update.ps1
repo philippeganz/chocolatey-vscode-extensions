e111749ffeacc5443626036489b5b6a6dc2090f1
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:33:51 +0200

    Add new wayou.vscode-todo-highlight extension

---
+++ automatic/vscode-todo-highlight/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "wayou"
+$ExtensionName = "vscode-todo-highlight"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
