130a806e00e83032582d908880349055ed746989
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:28:26 +0200

    Add new mhutchie.git-graph extension

---
+++ automatic/vscode-git-graph/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "mhutchie"
+$ExtensionName = "git-graph"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
