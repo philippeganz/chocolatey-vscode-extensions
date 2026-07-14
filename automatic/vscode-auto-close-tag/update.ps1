b927d4b16de91b7e0b858b4f041d02b1f8334ead
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:43 +0200

    Add new formulahendry.auto-close-tag extension

---
+++ automatic/vscode-auto-close-tag/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "formulahendry"
+$ExtensionName = "auto-close-tag"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
