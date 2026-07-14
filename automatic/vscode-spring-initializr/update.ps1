b0b9bf0167a4f364a76806aa3debf070ef29fbab
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:33:40 +0200

    Add new vscjava.vscode-spring-initializr extension

---
+++ automatic/vscode-spring-initializr/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "vscjava"
+$ExtensionName = "vscode-spring-initializr"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
