1b4445fcdc8e4049bd6c536d7fedb705d5b8ff98
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:26:03 +0200

    Add new codezombiech.gitignore extension

---
+++ automatic/vscode-gitignore/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "codezombiech"
+$ExtensionName = "gitignore"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
