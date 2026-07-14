beb236bc08fc4b2c4f352cccde981f1213c51d2a
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:30:00 +0200

    Add new msjsdiag.debugger-for-chrome extension

---
+++ automatic/vscode-debugger-for-chrome/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "msjsdiag"
+$ExtensionName = "debugger-for-chrome"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
