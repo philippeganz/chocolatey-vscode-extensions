d320086a01483d2b02731a5bb527fa057db0a08d
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:29:25 +0200

    Add new ms-dotnettools.csdevkit extension

---
+++ automatic/vscode-csdevkit/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-dotnettools"
+$ExtensionName = "csdevkit"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
