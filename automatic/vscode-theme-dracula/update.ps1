66c08c17f6d1eb8102b55f1351e3e443267af903
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:22 +0200

    Add new dracula-theme.theme-dracula extension

---
+++ automatic/vscode-theme-dracula/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "dracula-theme"
+$ExtensionName = "theme-dracula"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
