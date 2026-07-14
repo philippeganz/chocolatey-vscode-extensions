212f23be5d79f0fc5a89882c81ced1b73a825fa4
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:28:59 +0200

    Add new ms-ceintl.vscode-language-pack-ru extension

---
+++ automatic/vscode-language-pack-ru/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-ceintl"
+$ExtensionName = "vscode-language-pack-ru"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
