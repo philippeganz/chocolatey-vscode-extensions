a076db626b1c83af3cc56bae02020168fe26caa6
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:30 +0200

    Add new editorconfig.editorconfig extension

---
+++ automatic/vscode-editorconfig/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "editorconfig"
+$ExtensionName = "editorconfig"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
