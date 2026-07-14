20df1f6d0c4366fb2680cd672968c3c10b644eff
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:34:02 +0200

    Add new xdebug.php-pack extension

---
+++ automatic/vscode-php-pack/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "xdebug"
+$ExtensionName = "php-pack"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
