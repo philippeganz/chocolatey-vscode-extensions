ea0264c0217580360cd5f8063741a399daeb5444
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:27 +0200

    Add new dsznajder.es7-react-js-snippets extension

---
+++ automatic/vscode-es7-react-js-snippets/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "dsznajder"
+$ExtensionName = "es7-react-js-snippets"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
