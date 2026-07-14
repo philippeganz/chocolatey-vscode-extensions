53a3eed0acd38549628c8fc45133dd51580c35c2
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:33:33 +0200

    Add new tomoki1207.pdf extension

---
+++ automatic/vscode-pdf/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "tomoki1207"
+$ExtensionName = "pdf"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
