360216ca4cb0100f30f840fa5deaa88805a43e34
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:32 +0200

    Add new eg2.vscode-npm-script extension

---
+++ automatic/vscode-npm-script/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "eg2"
+$ExtensionName = "vscode-npm-script"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
