8201583e94fd7cc6cd5b89b4494fb7833084ec2e
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:25:38 +0200

    Add new angular.ng-template extension

---
+++ automatic/vscode-ng-template/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "angular"
+$ExtensionName = "ng-template"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
