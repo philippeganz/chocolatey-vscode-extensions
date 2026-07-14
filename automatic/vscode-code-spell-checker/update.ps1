949b23ae72a51c3cb7dced7e55ca9bdd40afb4a6
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:33:21 +0200

    Add new streetsidesoftware.code-spell-checker extension

---
+++ automatic/vscode-code-spell-checker/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "streetsidesoftware"
+$ExtensionName = "code-spell-checker"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
