7e5ee10c04c23a75e0ee5c97487a270d002fd495
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:34:09 +0200

    Add new zhuangtongfa.material-theme extension

---
+++ automatic/vscode-material-theme/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "zhuangtongfa"
+$ExtensionName = "material-theme"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
