8d7687451089db785ec2f726075a158e24ae3e94
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:29:28 +0200

    Add new ms-edgedevtools.vscode-edge-devtools extension

---
+++ automatic/vscode-edge-devtools/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-edgedevtools"
+$ExtensionName = "vscode-edge-devtools"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
