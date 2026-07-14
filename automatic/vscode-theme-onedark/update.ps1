a63e68011d98dabf681b3deb6b569ceec55728bf
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:25:29 +0200

    Add new akamud.vscode-theme-onedark extension

---
+++ automatic/vscode-theme-onedark/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "akamud"
+$ExtensionName = "vscode-theme-onedark"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
