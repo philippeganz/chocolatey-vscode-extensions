b441f5008a8b76296120b987afa8d9a0648112f6
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:33:17 +0200

    Add new sidthesloth.html5-boilerplate extension

---
+++ automatic/vscode-html5-boilerplate/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "sidthesloth"
+$ExtensionName = "html5-boilerplate"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
