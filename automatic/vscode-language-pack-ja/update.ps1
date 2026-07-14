6a8ed214d6e5b5e8061ecfaab9d65e2d07cd7839
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:28:50 +0200

    Add new ms-ceintl.vscode-language-pack-ja extension

---
+++ automatic/vscode-language-pack-ja/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-ceintl"
+$ExtensionName = "vscode-language-pack-ja"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
