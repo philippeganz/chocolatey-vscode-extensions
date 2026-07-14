3433f7911052983248f7837339d9538cba219ac4
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:33:27 +0200

    Add new techer.open-in-browser extension

---
+++ automatic/vscode-open-in-browser/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "techer"
+$ExtensionName = "open-in-browser"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
