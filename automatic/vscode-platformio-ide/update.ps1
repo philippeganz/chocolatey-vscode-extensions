a934114583282e781099c9a2b44ab7d9e69cc82c
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:32:46 +0200

    Add new platformio.platformio-ide extension

---
+++ automatic/vscode-platformio-ide/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "platformio"
+$ExtensionName = "platformio-ide"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
