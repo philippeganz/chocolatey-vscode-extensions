5f37122d074aefacdcd0f9cf418a4d8a97ddd82f
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:26:27 +0200

    Add new dart-code.flutter extension

---
+++ automatic/vscode-flutter/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "dart-code"
+$ExtensionName = "flutter"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
