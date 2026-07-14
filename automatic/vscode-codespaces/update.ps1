c6c005e204fe7a6389bf693c8cc85500b28006ec
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:51 +0200

    Add new github.codespaces extension

---
+++ automatic/vscode-codespaces/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "github"
+$ExtensionName = "codespaces"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
