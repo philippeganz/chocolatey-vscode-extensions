0110b3c28fa99341b2cf4442ecdb7abd0707140c
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:26:49 +0200

    Add new devsense.intelli-php-vscode extension

---
+++ automatic/vscode-intelli-php-vscode/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "devsense"
+$ExtensionName = "intelli-php-vscode"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
