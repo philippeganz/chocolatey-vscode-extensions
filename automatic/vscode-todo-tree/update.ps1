ef71c2e833d0fe86799020e1743b1b405e549c7a
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:28:08 +0200

    Add new gruntfuggly.todo-tree extension

---
+++ automatic/vscode-todo-tree/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "gruntfuggly"
+$ExtensionName = "todo-tree"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
