0efc191fc39b47badb74151fd7c88ee84bcfc9b2
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:26:07 +0200

    Add new coenraads.bracket-pair-colorizer extension

---
+++ automatic/vscode-bracket-pair-colorizer/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "coenraads"
+$ExtensionName = "bracket-pair-colorizer"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
