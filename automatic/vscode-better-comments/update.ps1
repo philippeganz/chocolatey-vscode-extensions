0788c31578db53d951c3de24a3d5c9299cedb1ec
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:25:23 +0200

    Add new aaron-bond.better-comments extension

---
+++ automatic/vscode-better-comments/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "aaron-bond"
+$ExtensionName = "better-comments"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
