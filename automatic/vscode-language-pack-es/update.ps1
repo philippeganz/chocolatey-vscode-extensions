351420d125af68800d10e5f98de90990b206666a
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:28:44 +0200

    Add new ms-ceintl.vscode-language-pack-es extension

---
+++ automatic/vscode-language-pack-es/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-ceintl"
+$ExtensionName = "vscode-language-pack-es"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
