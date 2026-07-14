c79cde5e471e45bedc90b7edec9b58d0878b8823
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:28:12 +0200

    Add new hbenl.vscode-test-explorer extension

---
+++ automatic/vscode-test-adapter-converter/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "ms-vscode"
+$ExtensionName = "test-adapter-converter"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
---
+++ automatic/vscode-test-explorer/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "hbenl"
+$ExtensionName = "vscode-test-explorer"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
