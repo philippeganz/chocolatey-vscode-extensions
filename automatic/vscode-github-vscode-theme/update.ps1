ebfb3670298efd473978918f049374cde81704e4
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:54 +0200

    Add new github.github-vscode-theme extension

---
+++ automatic/vscode-github-vscode-theme/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "github"
+$ExtensionName = "github-vscode-theme"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
