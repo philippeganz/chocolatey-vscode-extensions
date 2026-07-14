357d4d32f225b3c041e744b14a795a3e6f900e8f
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:26:01 +0200

    Add new christian-kohler.path-intellisense extension

---
+++ automatic/vscode-path-intellisense/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "christian-kohler"
+$ExtensionName = "path-intellisense"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
