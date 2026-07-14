9b063ab6e27d6b15e548bd1a15994fd87da38005
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:25:26 +0200

    Add new abusaidm.html-snippets extension

---
+++ automatic/vscode-html-snippets/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "abusaidm"
+$ExtensionName = "html-snippets"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
