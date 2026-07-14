6fa2eeca0f20d4bf68c8a9b1d7c6bb271872859b
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:06 +0200

    Add new donjayamanne.githistory extension

---
+++ automatic/vscode-githistory/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "donjayamanne"
+$ExtensionName = "githistory"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
