260b3e3d50e06198f97cca6b30219bb6fb1223c6
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:33:36 +0200

    Add new usernamehw.errorlens extension

---
+++ automatic/vscode-errorlens/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "usernamehw"
+$ExtensionName = "errorlens"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
