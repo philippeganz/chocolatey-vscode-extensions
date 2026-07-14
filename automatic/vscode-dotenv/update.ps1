f22ac5b71b738e1847c434fa42d5c7c879078a0c
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:28:29 +0200

    Add new mikestead.dotenv extension

---
+++ automatic/vscode-dotenv/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "mikestead"
+$ExtensionName = "dotenv"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
