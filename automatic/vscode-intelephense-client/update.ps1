3b682468f7cdff8c1957c7e4bd048181b4c0aa6e
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:25:46 +0200

    Add new bmewburn.vscode-intelephense-client extension

---
+++ automatic/vscode-intelephense-client/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "bmewburn"
+$ExtensionName = "vscode-intelephense-client"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
