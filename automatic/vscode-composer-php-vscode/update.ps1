bc840a1f429cfa66269c37b5dca53ab711fd6958
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:26:30 +0200

    Add new devsense.composer-php-vscode extension

---
+++ automatic/vscode-composer-php-vscode/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "devsense"
+$ExtensionName = "composer-php-vscode"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
