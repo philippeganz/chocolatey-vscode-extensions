b6712151ce9d62cfc63ecf3e6fbf1cc112256bc2
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:01 +0200

    Add new devsense.phptools-vscode extension

---
+++ automatic/vscode-phptools-vscode/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "devsense"
+$ExtensionName = "phptools-vscode"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
---
+++ automatic/vscode-profiler-php-vscode/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "devsense"
+$ExtensionName = "profiler-php-vscode"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
