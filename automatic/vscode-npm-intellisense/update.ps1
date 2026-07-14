93a12f4460a937c332e0e64909c78a1b40f97c53
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:25:58 +0200

    Add new christian-kohler.npm-intellisense extension

---
+++ automatic/vscode-npm-intellisense/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "christian-kohler"
+$ExtensionName = "npm-intellisense"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
