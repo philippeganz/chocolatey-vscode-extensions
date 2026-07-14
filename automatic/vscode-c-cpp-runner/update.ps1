b43f2b4e85f0006b2333aad9df33a9553d0a6443
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-14 16:27:47 +0200

    Add new franneck94.c-cpp-runner extension

---
+++ automatic/vscode-c-cpp-runner/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "franneck94"
+$ExtensionName = "c-cpp-runner"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
---
+++ automatic/vscode-lldb/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param()
+$ExtensionPublisher = "vadimcn"
+$ExtensionName = "vscode-lldb"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
