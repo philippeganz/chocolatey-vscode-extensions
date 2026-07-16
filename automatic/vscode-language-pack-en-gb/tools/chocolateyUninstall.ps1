2e522fad040e8710d10664a89daa830442e890ce
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-07-16 17:39:29 +0200

    Add official language packs

---
+++ automatic/vscode-language-pack-en-gb/tools/chocolateyUninstall.ps1
@@ -0,0 +1,11 @@
+<#
+.SYNOPSIS
+Auto-generated uninstall script for Chocolatey.
+
+.DESCRIPTION
+Removes the installed VSIX extension from VS Code.
+#>
+$ErrorActionPreference = 'Stop'
+
+Uninstall-VsCodeExtension -extensionId "ms-ceintl.vscode-language-pack-en-gb"
+
---
+++ automatic/vscode-language-pack-tr/update.ps1
@@ -0,0 +1,5 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Variables are declared for AU hooks and module scope but not read within this script block')]
+param()
+$ExtensionPublisher = "ms-ceintl"
+$ExtensionName = "vscode-language-pack-tr"
+. "$PSScriptRoot\..\..\bin\AuExtensionHooks.ps1"
