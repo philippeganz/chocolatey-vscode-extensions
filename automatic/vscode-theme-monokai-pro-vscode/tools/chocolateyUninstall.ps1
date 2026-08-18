54735d324fb7902fede9e34653f5482f0f7054d0
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-08-18 15:16:40 +0200

    Add new monokai.theme-monokai-pro-vscode extension

---
+++ automatic/vscode-theme-monokai-pro-vscode/tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+<#
+.SYNOPSIS
+Auto-generated uninstall script for Chocolatey.
+
+.DESCRIPTION
+Removes the installed VSIX extension from VS Code.
+#>
+$ErrorActionPreference = 'Stop'
+
+Uninstall-VsCodeExtension -extensionId "monokai.theme-monokai-pro-vscode"
