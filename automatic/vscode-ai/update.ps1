5950584dc30e98605f1a6e6b7c3eba71c7425bf9
Philippe Ganz <philippe.ganz@etat.ge.ch>
2026-09-15 09:34:03 +0200

    Remove vscode-ai extension

--- automatic/vscode-ai/update.ps1
+++
@@ -1,5 +0,0 @@
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '', Justification = 'Required for module state propagation')]
-param()
-$global:ExtensionPublisher = "ms-toolsai"
-$global:ExtensionName = "vscode-ai"
-. "$PSScriptRoot\..\..\bin\Update-ExtensionPackage.ps1"
